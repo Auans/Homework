495330be5880f32c13eee9204b5a7f3e16b65355
Thanakorn <eakauan001@gmail.com>
2021-07-28 04:44:03 +0700

    no message

--- homework2 take stick.py
+++ homework2 take stick.py
@@ -12,13 +12,22 @@ print("There are", N, "sticks in the pile")
 Name = input("What is your name: ")
 print()
 
+# magic number
+x = []
+for i in range (20):
+    a = 3*i-1 
+    x.append(a)      
+y = []
+for i in range (20):
+    b = 3*i 
+    y.append(b)  
+    
 for i in range(100):
 #player1 take stick
   take_stick =  int(input( str(Name) + ", How many sticks you will take (1 or 2): ")) 
   
   if (0 < take_stick <= 2) and (take_stick <= N) : 
-    N = N - take_stick    
-    
+    N = N - take_stick      
     if (N == 0):
       print(str(Name) + ", take the last stick ")  
       print()
@@ -28,30 +37,22 @@ for i in range(100):
     print()
     
 #player2 take stick
-    if N > 2:
-        if N == 5:  
+    if N > 0:
+        if N in x:
             bot_take = 1
-        elif N == 4:  
-            bot_take = 1
-        elif N == 3:
-            bot_take = 2
+        elif N in y: 
+            bot_take = 2  
         else:
-            bot_take = (random.randint(1,2))
-        N = N - bot_take
+            bot_take = (random.randint(1,2))   
+        N = N - bot_take 
         print("I, smart computer, take: ", bot_take)
         print("There are", N, "sticks in the pile")
-
-    elif N <= 2:
-        bot_take = 1   
-        N = N - bot_take
-        print("I, smart computer, take: ", bot_take)
-        print("There are", N, "sticks in the pile")
-        
-        if N == 0:
-            print("I, smart computer, take the last stick ")  
-            print()
-            print(str(Name) + " win (I, smart computer, am sad T_T) ")
-            break    
+        print()           
+    elif N == 0:  
+        print("I, smart computer, take the last stick ")  
+        print()
+        print(str(Name) + " win (I, smart computer, am sad T_T) ")
+        break    
     
   elif (take_stick < 0):
     print(" No you cannot take less than 1 stick !")
