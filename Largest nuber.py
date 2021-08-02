52c0db3399349816bb44a51695af8df948212bd9
Thanakorn <eakauan001@gmail.com>
2021-08-02 22:45:03 +0700

    no message

--- Largest nuber.py
+++ Largest nuber.py
@@ -11,7 +11,7 @@ def LargeNumber(number):
     
     for i in number:
         x = str(i)*2
-        List.append((x[:], i))
+        List.append((x, i))
         List.sort(reverse= True)
         
     for i in List:
