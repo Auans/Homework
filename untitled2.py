fc56e72db58d0fad4d1d4ffab4ed34272932db20
Thanakorn <eakauan001@gmail.com>
2021-09-07 05:04:20 +0700

    update topic number 5

--- untitled2.py
+++ untitled2.py
@@ -28,3 +28,17 @@ Syracuse_region[Syracuse_region['region'] == 'Syracuse' ].max()
 highest_price = avocado.groupby('region')[['AveragePrice']].max()
 highest_price.sort_values(by = 'AveragePrice')
 #region which sold the highest price of avocado in average is SanFrancisco 
+
+#4) Find the total amount of income (Avg_Price*Total_Volume) of each region.
+volume_price = avocado.groupby('region')[['Total Volume','AveragePrice']].max()
+income = volume_price['Total Volume']*volume_price['AveragePrice']
+
+#5) Let AVOCADO  Average Weight : 4046 => 4 ounces, 4225 => 9 ounces, 4770 => 12 ounces
+weight = avocado.groupby('region')[['4046', '4225','4770']].mean()
+number_4046 = weight['4046']/4
+number_4225 = weight['4225']/9
+number_4770 = weight['4770']/12
+number = pd.DataFrame({'number_4046':number_4046,'number_4225':number_4225,'number_4770':number_4770 })
+number['total number'] = number['number_4046'] +number['number_4225'] + number['number_4770']
+number.sort_values(by = 'total number')
+#The region that sold the largest of number of avocadoes is SouthCentral 
